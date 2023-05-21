b3390d910c5b3f5b3d79feba80452903a2d53dc2
Camilo5819 <cr7montealegre2010@gmail.com>
2023-05-21 13:40:16 -0500

    Added context to gpt model

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 // Require express, cors, dotenv, and routes
+const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
+const { Configuration, OpenAIApi } = require("openai");
 const cors = require("cors");
+const lds_input = fs.readFileSync("lds_input.txt", "utf8")
+// OpenAI API key
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
 
 // App uses cors and json and urlencoded middleware
 app.use(cors());
@@ -11,8 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // App routes
-app.get("/", (req, res) => {
-    res.send("Welcome to the backend!");
+app.get("/translate", async (req, res) => {
+    if(!req.query.text) {
+        return res.status(400).send("Missing text parameter");
+    }
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: lds_input}, {role: "assistant", content: "Entendido, a partir de ahora responderé en LSM. Por favor, proporciona la oración que deseas traducir."}, {role: "user", content: "\"" + req.query.text + "\""}],
+    });
+    res.status(response.status).send({
+        message: response.data.choices[0].message,
+        status: response.status,
+    });
 });
 
 app.listen(5000, () => {
